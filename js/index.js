cfa4bead575040410c55860abbd86b29ec5caba5
LaraManfredini1 <lmanfredini@udesa.edu.ar>
2023-05-30 01:06:44 -0300

    sumando index js

--- js/index.js
+++ js/index.js
@@ -21,3 +21,76 @@ else {
 }
 })
 
+//cada seccion tiene un url y un fetch propio
+
+//priurl
+let priurl = "https://api.deezer.com/track/3135556"
+fetch(priurl)
+.then(function(respuesta){
+return respuesta.json();
+})
+    .then(function(data){
+        let info = data.results /* el results es generico y contiene el array*/
+        let prisection = document.querySelector(".canciones")
+        let contenido = ""; /* comienza vacia porque es la lista a la que se le van a ir agregando cosas*/
+
+    for (let index = 0; index < info.length; index++){ /* i++ se va iterando*/
+        contenido+= /*hacer todo*/
+    
+    }
+        prisection.innerHTML += contenido
+
+    })
+    .catch(function(error){
+        console.log('El error es' + error);
+    })
+
+//segurl
+let segurl = "https://api.deezer.com/album/302127"
+fetch(segurl)
+.then(function(respuesta){
+    return respuesta.json();
+})
+
+.then(function(data){
+    console.log(data);
+    let info = data.results /* el results es generico y contiene el array*/
+    let segsection = document.querySelector(".albumes")
+    let contenido = ""; /* comienza vacia porque es la lista a la que se le van a ir agregando cosas*/
+
+    for (let index = 0; index < info.length; index++) { /*i++ se va iterando*/
+    contenido+= /*seguir aca*/
+
+    }
+    segsection.innerHTML += contenido
+})
+
+.catch(function(error){
+    console.log('El error es' + error);
+})
+
+//terurl
+let terurl = "https://api.deezer.com/artist/27"
+fetch(terurl)
+.then(function(respuesta){
+    return respuesta.json();
+})
+
+.then(function(data){
+    console.log(data);
+    let info = data.results /* el results es generico y contiene el array*/
+    let segsection = document.querySelector(".artistas")
+    let contenido = ""; /* comienza vacia porque es la lista a la que se le van a ir agregando cosas*/
+
+    for (let index = 0; index < info.length; index++) { /*i++ se va iterando*/
+    contenido+= /*seguir aca*/
+
+    }
+    tersection.innerHTML += contenido
+})
+
+.catch(function(error){
+    console.log('El error es' + error);
+})
+
+
