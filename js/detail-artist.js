d2405e8724299b80aaf01511d6b2945c09f0f904
LaraManfredini1 <lmanfredini@udesa.edu.ar>
2023-06-09 09:53:32 -0300

    cambios

--- js/detail-artist.js
+++ js/detail-artist.js
@@ -4,20 +4,55 @@ let campo = document.querySelector('.search')
 let aviso = document.querySelector('.alert')
 let formulario = document.querySelector('.formulario');
 
-formulario.addEventListener('submit', function(evento){  
+formulario.addEventListener('submit', function (evento) {
     evento.preventDefault()
     console.log("Error. Sin enviar")
-if (campo.value == "") {
-    aviso.innerText = 'El campo no puede estar vacío';
-    campo.style.outline = '2px solid red'
-    aviso.style.color = 'red'
-}
-else if (campo.value.length < 3){
-    aviso.innerText = 'Minimo 3 caracteres';
-    campo.style.outline = '2px solid red'
-    aviso.style.color = 'red'
-}
-else {
-    this.submit()
-}
+    if (campo.value == "") {
+        aviso.innerText = 'El campo no puede estar vacío';
+        campo.style.outline = '2px solid red'
+        aviso.style.color = 'red'
+    }
+    else if (campo.value.length < 3) {
+        aviso.innerText = 'Minimo 3 caracteres';
+        campo.style.outline = '2px solid red'
+        aviso.style.color = 'red'
+    }
+    else {
+        this.submit()
+    }
 })
+
+let queryString = location.search;
+let qsToObject = new URLSearchParams(queryString);
+let idArtista = qsToObject.get('id');
+
+let artistas1 = document.querySelector('.artist-details')
+
+fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/' + idArtista)
+    .then(function (res) {
+        return res.json();
+    })
+
+
+
+    .then(function (data) {
+        console.log(data);
+
+
+        artistas1.innerHTML += `
+  
+      <article class="artistbox">
+      <img src="${data.picture}">
+      <p>${data.title} </p>
+      <a href="./detail-artist.html"> ${data.artist.name}</a>
+      <a href="./detail-album.html"</a>
+      <br>
+      <a href="./detail-album.html">${data.album.title}</a>
+      </article>`
+
+
+    })
+
+    .catch(function (error) {
+        console.log(error);
+    })
